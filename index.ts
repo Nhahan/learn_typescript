72171ce7b34e8bd05bedc64d332dd694bdc7bf60
Nhahan <kisy324@naver.com>
2021-10-29 20:43:16 +0900

    typescipt settings

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("hello")
+const nickname = "nini"
+const age = 24
+const gender = "male"
+
+const sayHi = (nickname, age, gender) => {
+  console.log(`Hello ${nickname}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi(nickname, age, gender)
