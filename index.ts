952a3615dfce29aa830a3f0e612002e3f791b4af
Nhahan <kisy324@naver.com>
2021-10-29 20:48:48 +0900

    return

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ const nickname = "nini"
 const age = 24
 const gender = "male"
 
-const sayHi = (nickname: string, age: number, gender: string) => {
-  console.log(`Hello ${nickname}, you are ${age}, you are a ${gender}`)
+const sayHi = (nickname: string, age: number, gender: string): string => {
+  return `Hello ${nickname}, you are ${age}, you are a ${gender}`
 }
 
-sayHi(nickname, age, gender)
+console.log(sayHi(nickname, age, gender))
